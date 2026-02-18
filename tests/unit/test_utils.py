67711d40fe2dcc713ef55a172db77e5b3ee2dbbc
Your Name <you@example.com>
2026-02-18 19:27:30 +0100

    adding tests and github actions

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -3,7 +3,10 @@ import os
 import tempfile
 import json
 import pytest
-from templating.utils import load_json, load_data , get_default_browser_windows
+from unittest.mock import patch, MagicMock
+import types
+from unittest import mock
+from templating.utils import load_json, load_data , get_default_browser_windows, open_in_browser
 
 @pytest.mark.skipif(sys.platform != "win32", reason="Windows-specific test")
 def test_load_json_and_load_data(tmp_path):
@@ -36,19 +39,16 @@ def test_load_json_invalid_json(tmp_path):
         load_json(str(bad_file))
 
 
-import types
-from unittest import mock
-
-# @pytest.mark.skipif(sys.platform != "win32", reason="Windows-specific test")
-# def test_get_default_browser_windows_success():
-#     # Mock winreg.OpenKey and winreg.QueryValueEx to simulate registry access
-#     with mock.patch("templating.utils.winreg.OpenKey") as mock_openkey, \
-#          mock.patch("templating.utils.winreg.QueryValueEx") as mock_queryvalueex:
-#         mock_key = mock.MagicMock()
-#         mock_openkey.return_value.__enter__.return_value = mock_key
-#         mock_queryvalueex.return_value = ("ChromeHTML", None)
-#         result = get_default_browser_windows()
-#         assert result == "ChromeHTML"
+@pytest.mark.skipif(sys.platform != "win32", reason="Windows-specific test")
+def test_get_default_browser_windows_success():
+    # Mock winreg.OpenKey and winreg.QueryValueEx to simulate registry access
+    with mock.patch("templating.utils.winreg.OpenKey") as mock_openkey, \
+        mock.patch("templating.utils.winreg.QueryValueEx") as mock_queryvalueex:
+        mock_key = mock.MagicMock()
+        mock_openkey.return_value.__enter__.return_value = mock_key
+        mock_queryvalueex.return_value = ("ChromeHTML", None)
+        result = get_default_browser_windows()
+        assert result == "ChromeHTML"
 
 
 @pytest.mark.skipif(sys.platform != "win32", reason="Windows-specific test")
@@ -57,3 +57,21 @@ def test_get_default_browser_windows_registry_error():
     with mock.patch("templating.utils.winreg.OpenKey", side_effect=OSError("Registry not found")):
         with pytest.raises(OSError):
             get_default_browser_windows()
+
+
+@patch("os.path.isfile")
+def test_browser_not_found(mock_isfile):
+    # Browser missing
+    mock_isfile.side_effect = [False]
+
+    with pytest.raises(FileNotFoundError):
+        open_in_browser("chrome", "bad_path.exe", "index.html")
+
+
+@patch("os.path.isfile")
+def test_html_not_found(mock_isfile):
+    # Browser exists, file missing
+    mock_isfile.side_effect = [True, False]
+
+    with pytest.raises(FileNotFoundError):
+        open_in_browser("chrome", "chrome.exe", "missing.html")
